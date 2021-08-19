340aaf330d9c91ded37a2e420dd91ea584a404c6
suele <suelen.ads.guimaraes@gmail.com>
2021-08-19 15:36:28 -0300

    Adiciona paginação a API.

--- bazar/src/main/java/com/bazar/api/bazar/resources/ProductResource.java
+++ bazar/src/main/java/com/bazar/api/bazar/resources/ProductResource.java
@@ -3,10 +3,11 @@ package com.bazar.api.bazar.resources;
 import com.bazar.api.bazar.entities.Product;
 import com.bazar.api.bazar.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class ProductResource {
@@ -14,7 +15,13 @@ public class ProductResource {
     private ProductRepository productRepository;
 
     @GetMapping("/products")
-    public List<Product> products(){
-        return productRepository.findAll();
+    public ResponseEntity<Page<Product>> products (Pageable pageable) {
+
+        if (pageable.isPaged()) {
+            return ResponseEntity.ok().body(productRepository.findAll(pageable));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
     }
 }

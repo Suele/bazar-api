757b55a3c7f075e1f64708750b2753e106a4c890
suele <suelen.ads.guimaraes@gmail.com>
2021-08-20 17:47:15 -0300

    Adiciona busca por id.

--- bazar/src/main/java/com/bazar/api/bazar/resources/ProductResource.java
+++ bazar/src/main/java/com/bazar/api/bazar/resources/ProductResource.java
@@ -5,9 +5,12 @@ import com.bazar.api.bazar.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class ProductResource {
@@ -22,6 +25,14 @@ public class ProductResource {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
 
+    @GetMapping("/product/{id}")
+    public ResponseEntity getProductId (@PathVariable("id") Long id) {
+        if (productRepository.existsById(id)) {
+            return ResponseEntity.ok().body(productRepository.findById(id));
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"O id do produto pesquisado não existe");
+        }
     }
 }

716894b19282387d6680d602d259939a4b2265f2
suele <suelen.ads.guimaraes@gmail.com>
2021-09-01 14:52:26 -0300

    Adiciona o arquivo para inserir dados na tabela product.

---
+++ bazar/create-insert-sql/insert-table.sql
@@ -0,0 +1,6 @@
+----Inserir dados na tabela product---
+INSERT INTO product(name_product, quantity, product_type, description, value_for_sale, category_id) VALUES('cadeira giratória', 3, 'cadeira', 'com rodinha cor preta',890.99, 1);
+
+INSERT INTO product(name_product, quantity, product_type, description, value_for_sale, category_id) VALUES('mesa de madeira', 2, 'mesa', 'mesa cor mogno', 1000.99, 1);
+
+INSERT INTO product(name_product, quantity, product_type, description, value_for_sale, category_id) VALUES('teclado luminoso', 5, 'teclado', 'teclado preto', 199.90, 2);

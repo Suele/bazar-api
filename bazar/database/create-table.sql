bb14446087931ca5487ff7c9f65109515a554f47
suele <suelen.ads.guimaraes@gmail.com>
2021-09-15 18:46:47 -0300

    Adiciona comentarios ao arquivo e ajusta os nomes das colunas nas tabelas produto, marca e categoria.

--- bazar/database/create-table.sql
+++ bazar/database/create-table.sql
@@ -1,13 +1,25 @@
----marca---
+---------------Este arquivo contem todas os scripts SQL para criar as tabelas utilizadas no projeto Bazar-------------
+
+---------------Cria a tabela Marca de Produto---------------
 CREATE TABLE IF NOT EXISTS brand(
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(15) NOT NULL
+	brand_id SERIAL PRIMARY KEY,
+	brand_name VARCHAR(18) NOT NULL UNIQUE
 );
 
----categoria----
-CREATE TABLE IF NOT EXISTS category(
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(18) NOT NULL UNIQUE
+----------------Cria a tabela de categoria-------------------
+CREATE TABLE category(
+	category_id SERIAL PRIMARY KEY,
+	category_name VARCHAR(19) NOT NULL UNIQUE
+);
+
+----------------Cria a tabela de Produto----------------------
+CREATE TABLE IF NOT EXISTS product(
+	product_id SERIAL PRIMARY KEY,
+	product_name VARCHAR(23) NOT NULL,
+	quantity smallint NOT NULL,
+	description VARCHAR(35) NOT NULL,
+	value_for_sale NUMERIC(7,2) NOT NULL,
+	category_id SERIAL REFERENCES category(category_id) ON delete CASCADE ON update CASCADE
 );
 
 ---cliente---
@@ -16,8 +28,8 @@ CREATE TABLE IF NOT EXISTS client(
 	name VARCHAR(30) NOT NULL,
 	birthaday date NOT NULL,
 	cpf VARCHAR(11) NOT NULL UNIQUE,
-	phone_id SERIAL REFERENCES phone(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	address_id SERIAL REFERENCES address(id) ON DELETE CASCADE ON UPDATE CASCADE
+	phone_id SERIAL REFERENCES phone(id) ON delete CASCADE ON update CASCADE,
+	address_id SERIAL REFERENCES address(id) ON delete CASCADE ON update CASCADE
 );
 
 ---telefone---
@@ -44,24 +56,14 @@ CREATE TABLE IF NOT EXISTS provider(
 	cnpj VARCHAR(14) NOT NULL
 );
 
----produto----
-CREATE TABLE IF NOT EXISTS product(
-	product_id SERIAL PRIMARY KEY,
-	name_product VARCHAR(20) NOT NULL,
-	quantity smallint NOT NULL,
-	description VARCHAR(30) NOT NULL,
-	value_for_sale NUMERIC(7,2) NOT NULL,
-	category_id SERIAL REFERENCES category(id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
 --------Itens da venda---
 -------relacionamento entre produto e venda surge outra tabela-----
 ------um produto pode estar em uma venda ou muitos------
 ------uma venda pode ter um produto ou muitos produtos-----
 CREATE TABLE IF NOT EXISTS items_sale(
 items_sale_id SERIAL PRIMARY KEY,
-product_id REFERENCES product(product_id) ON DELETE CASCADE ON UPDATE CASCADE,
-sale_id REFERENCES sale(sale_id) ON DELETE CASCADE ON UPDATE CASCADE
+product_id REFERENCES product(product_id) ON delete CASCADE ON update CASCADE,
+sale_id REFERENCES sale(sale_id) ON delete CASCADE ON update CASCADE
 );
 
 ---venda----
@@ -75,14 +77,14 @@ CREATE TABLE IF NOT EXISTS sale(
 ---por ser um relacionamento ManyToMany----
 CREATE TABLE product_provider(
 	id SERIAL PRIMARY KEY,
-	product_id SERIAL REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	provider_id SERIAL REFERENCES provider(id) ON DELETE CASCADE ON UPDATE CASCADE
+	product_id SERIAL REFERENCES product(id) ON delete CASCADE ON update CASCADE,
+	provider_id SERIAL REFERENCES provider(id) ON delete CASCADE ON update CASCADE
 );
 
 ---relacionamento entre product e brand surge outra tabela---
 ---por ser um relacionamento ManyToMany----
 CREATE TABLE product_brand(
 	id SERIAL PRIMARY KEY,
-	product_id SERIAL REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	brand_id SERIAL REFERENCES brand(id) ON DELETE CASCADE ON UPDATE CASCADE
+	product_id SERIAL REFERENCES product(id) ON delete CASCADE ON update CASCADE,
+	brand_id SERIAL REFERENCES brand(id) ON delete CASCADE ON update CASCADE
 );

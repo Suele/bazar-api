66bab64d6cb51d083a8d9c573e26baaee24dabdc
suele <suelen.ads.guimaraes@gmail.com>
2021-09-16 00:27:16 -0300

    Ajusta as colunas nas tabelas provider,product_provider, product_brand, sale e items_sale.

--- bazar/database/create-table.sql
+++ bazar/database/create-table.sql
@@ -22,8 +22,50 @@ CREATE TABLE IF NOT EXISTS product(
 	category_id SERIAL REFERENCES category(category_id) ON delete CASCADE ON update CASCADE
 );
 
+----------------Cria a tabela de fornecedor--------------------------
+CREATE TABLE IF NOT EXISTS provider(
+	provider_id SERIAL PRIMARY KEY,
+	provider_name VARCHAR(61) NOT NULL,
+	cnpj VARCHAR(14) NOT NULL UNIQUE
+);
+
+----------------Cria a tabela para o relacionamento entre fornecedor e produto---------------
+----------------relacionamento entre product e provider surge outra tabela-------------------
+-------------------por ser um relacionamento ManyToMany----------------------------
+CREATE TABLE product_provider(
+	id SERIAL PRIMARY KEY,
+	product_id SERIAL REFERENCES product(product_id) ON delete CASCADE ON update CASCADE,
+	provider_id SERIAL REFERENCES provider(provider_id) ON delete CASCADE ON update CASCADE
+);
+
+--------------------Cria a tabela para o relacionamento entre produto e marca---------------
+---------------------relacionamento entre product e brand surge outra tabela----------------
+--------------------------por ser um relacionamento ManyToMany-------------------------
+CREATE TABLE IF NOT EXISTS product_brand(
+	id SERIAL PRIMARY KEY,
+	product_id SERIAL REFERENCES product(product_id) ON delete CASCADE ON update CASCADE,
+	brand_id SERIAL REFERENCES brand(brand_id) ON delete CASCADE ON update CASCADE
+);
+
+----------------------Cria a tabela de venda-----------------
+CREATE TABLE IF NOT EXISTS sale(
+	sale_id SERIAL PRIMARY KEY,
+	sale_date date DEFAULT Now() NOT NULL,
+	total NUMERIC(7,2) NOT NULL
+);
+
+---------------------Cria a tabela para o relacionamento entre produto e venda----------------------
+-----------------------relacionamento entre produto e venda surge outra tabela---------------------
+----------------------------------por ser um relacionamento ManyToMany----------------------------
+CREATE TABLE IF NOT EXISTS items_sale(
+    items_sale_id SERIAL PRIMARY KEY,
+    product_id SERIAL REFERENCES product(product_id) ON delete CASCADE ON update CASCADE,
+    sale_id SERIAL REFERENCES sale(sale_id) ON delete CASCADE ON update CASCADE
+);
+
+--------------------------------------------------------------------------------------------------------------------
 ---cliente---
-CREATE TABLE IF NOT EXISTS client(
+create TABLE IF NOT EXISTS client(
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(30) NOT NULL,
 	birthaday date NOT NULL,
@@ -33,58 +75,18 @@ CREATE TABLE IF NOT EXISTS client(
 );
 
 ---telefone---
-CREATE TABLE IF NOT EXISTS phone(
+create TABLE IF NOT EXISTS phone(
 	id SERIAL PRIMARY KEY,
 	area_code smallint NOT NULL,
 	phone_number VARCHAR(11) NOT NULL UNIQUE
 );
 
 ---endereco---
-CREATE TABLE IF NOT EXISTS address(
+create TABLE IF NOT EXISTS address(
 	id SERIAL PRIMARY KEY,
 	street VARCHAR(20) NOT NULL,
 	number_street smallint NOT NULL,
 	zipcode VARCHAR(9) NOT NULL,
 	city VARCHAR(18) NOT NULL,
 	state VARCHAR(21) NOT NULL
-);
-
----fornecedor----
-CREATE TABLE IF NOT EXISTS provider(
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(25) NOT NULL,
-	cnpj VARCHAR(14) NOT NULL
-);
-
---------Itens da venda---
--------relacionamento entre produto e venda surge outra tabela-----
-------um produto pode estar em uma venda ou muitos------
-------uma venda pode ter um produto ou muitos produtos-----
-CREATE TABLE IF NOT EXISTS items_sale(
-items_sale_id SERIAL PRIMARY KEY,
-product_id REFERENCES product(product_id) ON delete CASCADE ON update CASCADE,
-sale_id REFERENCES sale(sale_id) ON delete CASCADE ON update CASCADE
-);
-
----venda----
-CREATE TABLE IF NOT EXISTS sale(
-	sale_id SERIAL PRIMARY KEY,
-	sale_date date DEFAULT Now() NOT NULL,
-	total NUMERIC(7,2) NOT NULL
-);
-
----relacionamento entre product e provider surge outra tabela---
----por ser um relacionamento ManyToMany----
-CREATE TABLE product_provider(
-	id SERIAL PRIMARY KEY,
-	product_id SERIAL REFERENCES product(id) ON delete CASCADE ON update CASCADE,
-	provider_id SERIAL REFERENCES provider(id) ON delete CASCADE ON update CASCADE
-);
-
----relacionamento entre product e brand surge outra tabela---
----por ser um relacionamento ManyToMany----
-CREATE TABLE product_brand(
-	id SERIAL PRIMARY KEY,
-	product_id SERIAL REFERENCES product(id) ON delete CASCADE ON update CASCADE,
-	brand_id SERIAL REFERENCES brand(id) ON delete CASCADE ON update CASCADE
 );

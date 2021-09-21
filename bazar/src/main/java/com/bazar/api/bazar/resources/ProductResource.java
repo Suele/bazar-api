870b4a7c1092a93a8ffa99c387724f5b1cc29cab
suele <suelen.ads.guimaraes@gmail.com>
2021-09-20 21:16:23 -0300

    Adiciona as configurações para utilizar o swagger.

--- bazar/src/main/java/com/bazar/api/bazar/resources/ProductResource.java
+++ bazar/src/main/java/com/bazar/api/bazar/resources/ProductResource.java
@@ -3,6 +3,8 @@ package com.bazar.api.bazar.resources;
 import com.bazar.api.bazar.Carrinho;
 import com.bazar.api.bazar.entities.Product;
 import com.bazar.api.bazar.services.ProductService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -11,27 +13,32 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Min;
 
+@Api(tags = "produto")
 @RestController
 @Validated
 public class ProductResource {
     @Autowired
     private ProductService productService;
 
+    @ApiOperation(value = "products")
     @GetMapping("/products/")
     public ResponseEntity<Page<Product>> products () {
         return ResponseEntity.ok().body(productService.getAllProduct());
     }
 
+    @ApiOperation(value = "getProductId")
     @GetMapping("/product/{id}")
     public ResponseEntity<Product> getProductId (@PathVariable(value = "id") @Min(value = 1, message = "id invalido.") Long id) {
         return ResponseEntity.ok().body(productService.getId(id));
     }
 
+    @ApiOperation(value = "getProductName")
     @GetMapping("/products/{productName}")
     public ResponseEntity<?> getProductName (@PathVariable(value = "productName") String productName) {
         return ResponseEntity.ok().body(productService.getAllProductName(productName));
     }
 
+    @ApiOperation(value = "itemsCart")
     @PostMapping("/items_cart")
     public ResponseEntity<Carrinho> itemsCart (@RequestBody Product product) {
         return ResponseEntity.ok().body(productService.itemsCart(product));
